b0ae639e5b781129edbefd86954e8e9eb66cc6b2
Manoj <manojpatil9147@gmail.com>
2024-06-21 08:32:32 +0530

    updated

--- networking.py
+++ networking.py
@@ -69,7 +69,7 @@ def start_server(ipv6_address, port, stop_event):
                 print(section_no)
                 for section in section_no:
                     sheet = wb[sheets[section]]
-                    # print(sheet)
+                    print(sheet)
                     # print(received_data.get('results').get(str(section)))
                     sheet.append(received_data.get('results').get(str(section)))
             wb.save("resources/"+received_data.get('class_name')+'.xlsx')
